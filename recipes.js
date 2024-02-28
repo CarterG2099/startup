7d4a887deeff618720ec0acd6c52d05c8f881e9f
CarterG2099 <gividenc@byu.edu>
2024-02-27 19:44:42 -0700

    added instructions modal

--- recipes.js
+++ recipes.js
@@ -1,177 +1,188 @@
-class Recipes {
-    constructor() {
-        const userNameEl = document.querySelector('.user-name');
-        userNameEl.textContent = this.getUserName() + ' ' + userNameEl.textContent;
-        this.generateTable(this.breakfastData, 'breakfast-table');
-        this.generateTable(this.lunchData, 'lunch-table');
-        this.generateTable(this.dinnerData, 'dinner-table');
-    }
+    class Recipes {
+        constructor() {
+            const userNameEl = document.querySelector('.user-name');
+            userNameEl.textContent = this.getUserName() + ' ' + userNameEl.textContent;
+            this.generateTable(this.breakfastData, 'breakfast-table');
+            this.generateTable(this.lunchData, 'lunch-table');
+            this.generateTable(this.dinnerData, 'dinner-table');
+        }
 
-    getUserName() {
-        return localStorage.getItem('userName') ?? 'UserName';
-    }
+        getUserName() {
+            return localStorage.getItem('userName') ?? 'UserName';
+        }
 
-    showModal() {
-        const modalEl = document.querySelector('#myModal');
-        const modal = new bootstrap.Modal(modalEl);
-        modalEl.show()
-    }
+        showModal() {
+            const modalEl = document.querySelector('#myModal');
+            const modal = new bootstrap.Modal(modalEl);
+            modalEl.show()
+        }
 
-    // increaseVoteCount() {
-    //     const countEl = document.querySelector('.count');
-    //     countEl.textContent = parseInt(countEl.textContent) + 1;
-    // }
+        showInstructionsModal(instructions) {
+            const modalEl = document.querySelector('#instructionsModal');
+            const modalBody = modalEl.querySelector('.modal-body');
+            modalBody.textContent = instructions;
 
-    createRow(meal) {
-        const rows = [];
+            const modal = new bootstrap.Modal(modalEl);
+            modal.show()
+        }
 
-        // Title row
-        const firstRow = document.createElement('tr');
-        firstRow.className = 'meal-row';
-        const titleCell = document.createElement('td');
-        titleCell.textContent = meal.mealTitle;
-        titleCell.className = 'meal-title';
-        firstRow.appendChild(titleCell);
+        // increaseVoteCount() {
+        //     const countEl = document.querySelector('.count');
+        //     countEl.textContent = parseInt(countEl.textContent) + 1;
+        // }
 
-        // Ingredients row
-        const ingredientsCell = document.createElement('td');
-        const ingredientsList = document.createElement('ul');
-        meal.ingredients.forEach((ingredient) => {
-            const li = document.createElement('li');
-            li.textContent = ingredient;
-            ingredientsList.appendChild(li);
-        });
-        ingredientsCell.appendChild(ingredientsList);
-        firstRow.appendChild(ingredientsCell);
+        createRow(meal) {
+            const rows = [];
 
-        // Per Serving row
-        const perServingCell = document.createElement('td');
-        const perServingList = document.createElement('ul');
-        Object.entries(meal.perServing).forEach(([key, value]) => {
-            const li = document.createElement('li');
-            li.textContent = `${key}: ${value}`;
-            perServingList.appendChild(li);
-        });
-        perServingCell.appendChild(perServingList);
-        firstRow.appendChild(perServingCell);
-        rows.push(firstRow);
+            // Title row
+            const firstRow = document.createElement('tr');
+            firstRow.className = 'meal-row';
+            const titleCell = document.createElement('td');
+            titleCell.textContent = meal.mealTitle;
+            titleCell.className = 'meal-title';
+            firstRow.appendChild(titleCell);
 
-        // Instructions row
-        const secondRow = document.createElement('tr');
-        secondRow.className = 'link-row';
-        const instructionsCell = document.createElement('td');
-        const instructionsLink = document.createElement('a');
-        instructionsLink.href = meal.instructionsLink;
-        instructionsLink.textContent = 'Instructions';
-        instructionsCell.appendChild(instructionsLink);
-        secondRow.appendChild(instructionsCell);
+            // Ingredients row
+            const ingredientsCell = document.createElement('td');
+            const ingredientsList = document.createElement('ul');
+            meal.ingredients.forEach((ingredient) => {
+                const li = document.createElement('li');
+                li.textContent = ingredient;
+                ingredientsList.appendChild(li);
+            });
+            ingredientsCell.appendChild(ingredientsList);
+            firstRow.appendChild(ingredientsCell);
 
-        // Reviews row
-        const reviewsCell = document.createElement('td');
-        const reviewsLink = document.createElement('a');
-        reviewsLink.href = meal.reviewsLink;
-        reviewsLink.textContent = 'Reviews';
-        reviewsCell.appendChild(reviewsLink);
-        secondRow.appendChild(reviewsCell);
+            // Per Serving row
+            const perServingCell = document.createElement('td');
+            const perServingList = document.createElement('ul');
+            Object.entries(meal.perServing).forEach(([key, value]) => {
+                const li = document.createElement('li');
+                li.textContent = `${key}: ${value}`;
+                perServingList.appendChild(li);
+            });
+            perServingCell.appendChild(perServingList);
+            firstRow.appendChild(perServingCell);
+            rows.push(firstRow);
 
-        // Votes row
-        const votesCell = document.createElement('td');
-        votesCell.textContent = 'Votes: ';
-        const upvoteButton = document.createElement('button');
-        upvoteButton.className = 'btn btn-primary';
-        upvoteButton.textContent = 'UpVote';
-        upvoteButton.addEventListener('click', () => this.upVoteCounts());
-        votesCell.appendChild(upvoteButton);
-        secondRow.appendChild(votesCell);
-        rows.push(secondRow);
+            // Instructions row
+            const secondRow = document.createElement('tr');
+            secondRow.className = 'link-row';
+            const instructionsCell = document.createElement('td');
+            const instructionsLink = document.createElement('a');
+            instructionsLink.href = '#';
+            instructionsLink.textContent = 'Instructions';
+            instructionsLink.addEventListener('click', () => this.showInstructionsModal(meal.instructions));
+            instructionsCell.appendChild(instructionsLink);
+            secondRow.appendChild(instructionsCell);
 
-        return rows;
-    }
+            // Reviews row
+            const reviewsCell = document.createElement('td');
+            const reviewsLink = document.createElement('a');
+            reviewsLink.href = meal.reviewsLink;
+            reviewsLink.textContent = 'Reviews';
+            reviewsCell.appendChild(reviewsLink);
+            secondRow.appendChild(reviewsCell);
 
-    generateTable(mealData, id) {
-        const tableContainer = document.getElementById(id);
-        const table = document.createElement('table');
-        table.className = 'table';
-        const thead = document.createElement('thead');
-        const tbody = document.createElement('tbody');
+            // Votes row
+            const votesCell = document.createElement('td');
+            votesCell.textContent = 'Votes: ';
+            const upvoteButton = document.createElement('button');
+            upvoteButton.className = 'btn btn-primary';
+            upvoteButton.textContent = 'UpVote';
+            upvoteButton.addEventListener('click', () => this.upVoteCounts());
+            votesCell.appendChild(upvoteButton);
+            secondRow.appendChild(votesCell);
+            rows.push(secondRow);
 
-        const headerRow = document.createElement('tr');
-        ['Meal Title', 'Ingredients', 'Nutrition Info'].forEach((header) => {
-          const th = document.createElement('th');
-          th.textContent = header;
-          headerRow.appendChild(th);
-        });
-        thead.appendChild(headerRow);
+            return rows;
+        }
 
-        mealData.forEach((meal) => {
-          const mealRows = this.createRow(meal);
-          mealRows.forEach((row) => tbody.appendChild(row));
-        });
+        generateTable(mealData, id) {
+            const tableContainer = document.getElementById(id);
+            const table = document.createElement('table');
+            table.className = 'table';
+            const thead = document.createElement('thead');
+            const tbody = document.createElement('tbody');
 
-        table.appendChild(thead);
-        table.appendChild(tbody);
-        tableContainer.appendChild(table);
-    }
+            const headerRow = document.createElement('tr');
+            ['Meal Title', 'Ingredients', 'Nutrition Info'].forEach((header) => {
+            const th = document.createElement('th');
+            th.textContent = header;
+            headerRow.appendChild(th);
+            });
+            thead.appendChild(headerRow);
 
-    breakfastData = [
-        {
-            mealTitle: 'Waffles',
-            ingredients: ['ingredient1', 'ingredient2', 'ingredient3'],
-            perServing: {
-                calories: '300',
-                protein: '20g',
-                fat: '10g',
-                carbs: '40g',
+            mealData.forEach((meal) => {
+            const mealRows = this.createRow(meal);
+            mealRows.forEach((row) => tbody.appendChild(row));
+            });
+
+            table.appendChild(thead);
+            table.appendChild(tbody);
+            tableContainer.appendChild(table);
+        }
+
+        breakfastData = [
+            {
+                mealTitle: 'Waffles',
+                ingredients: ['ingredient1', 'ingredient2', 'ingredient3'],
+                perServing: {
+                    calories: '300',
+                    protein: '20g',
+                    fat: '10g',
+                    carbs: '40g',
+                },
+                instructions: 'Sample instructions',
+                reviewsLink: 'reviews.html',
+                votes: "0"
             },
-            instructions: 'Sample instructions',
-            reviewsLink: 'reviews.html',
-        },
-        {
-            mealTitle: 'Eggs',
-            ingredients: ['ingredient1', 'ingredient2', 'ingredient3'],
-            perServing: {
-                calories: '300',
-                protein: '20g',
-                fat: '10g',
-                carbs: '40g',
+            {
+                mealTitle: 'Eggs',
+                ingredients: ['ingredient1', 'ingredient2', 'ingredient3'],
+                perServing: {
+                    calories: '300',
+                    protein: '20g',
+                    fat: '10g',
+                    carbs: '40g',
+                },
+                instructions: 'Sample instructions',
+                reviewsLink: 'reviews.html',
             },
-            instructions: 'Sample instructions',
-            reviewsLink: 'reviews.html',
-        },
-        // Add more data as needed
-    ];
-    lunchData = [
-        {
-            mealTitle: 'Pasta',
-            ingredients: ['ingredient1', 'ingredient2', 'ingredient3'],
-            perServing: {
-                calories: '300',
-                protein: '20g',
-                fat: '10g',
-                carbs: '40g',
+            // Add more data as needed
+        ];
+        lunchData = [
+            {
+                mealTitle: 'Pasta',
+                ingredients: ['ingredient1', 'ingredient2', 'ingredient3'],
+                perServing: {
+                    calories: '300',
+                    protein: '20g',
+                    fat: '10g',
+                    carbs: '40g',
+                },
+                instructions: 'Sample instructions',
+                reviewsLink: 'reviews.html',
             },
-            instructions: 'Sample instructions',
-            reviewsLink: 'reviews.html',
-        },
-        // Add more data as needed
-    ];
+            // Add more data as needed
+        ];
 
 
-    dinnerData = [
-        {
-            mealTitle: 'Fajitas',
-            ingredients: ['ingredient1', 'ingredient2', 'ingredient3'],
-            perServing: {
-                calories: '300',
-                protein: '20g',
-                fat: '10g',
-                carbs: '40g',
+        dinnerData = [
+            {
+                mealTitle: 'Fajitas',
+                ingredients: ['ingredient1', 'ingredient2', 'ingredient3'],
+                perServing: {
+                    calories: '300',
+                    protein: '20g',
+                    fat: '10g',
+                    carbs: '40g',
+                },
+                instructions: 'Sample instructions',
+                reviewsLink: 'reviews.html',
             },
-            instructions: 'Sample instructions',
-            reviewsLink: 'reviews.html',
-        },
-        // Add more data as needed
-    ];
-}
+            // Add more data as needed
+        ];
+    }
 
-const recipes = new Recipes();
+    const recipes = new Recipes();
